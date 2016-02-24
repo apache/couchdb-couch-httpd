75f19a3825dfb11dc6e0e7187cb102ecd5e43f2c
ILYA Khlopotov <iilyak@ca.ibm.com>
2016-02-24 07:19:12 -0800

    Rename chttpd_config_listener into couch_httpd_config_listener

--- src/couch_httpd_config_listener.erl
+++ src/couch_httpd_config_listener.erl
@@ -10,7 +10,7 @@
 % License for the specific language governing permissions and limitations under
 % the License.
 
--module(chttpd_config_listener).
+-module(couch_httpd_config_listener).
 -vsn(2).
 -behaviour(config_listener).
 
