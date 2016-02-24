87e11270a6f58047f5440793237ad6dbbf6bfcfc
ILYA Khlopotov <iilyak@ca.ibm.com>
2016-02-24 07:09:53 -0800

    Rename chttpd_error_info_tests into couch_httpd_error_info_test

--- test/couch_httpd_error_info_test.erl
+++ test/couch_httpd_error_info_test.erl
@@ -10,7 +10,7 @@
 % License for the specific language governing permissions and limitations under
 % the License.
 
--module(chttpd_error_info_tests).
+-module(couch_httpd_error_info_test).
 
 -include_lib("eunit/include/eunit.hrl").
 
