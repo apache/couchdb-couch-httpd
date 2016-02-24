ec0386255e97f9ba66ef177a6b9c8813bcd7f0a4
ILYA Khlopotov <iilyak@ca.ibm.com>
2016-02-24 07:03:33 -0800

    Rename chttpd into couch_httpd

--- src/couch_httpd.erl
+++ src/couch_httpd.erl
@@ -10,7 +10,7 @@
 % License for the specific language governing permissions and limitations under
 % the License.
 
--module(chttpd).
+-module(couch_httpd).
 -include_lib("couch/include/couch_db.hrl").
 -include_lib("chttpd/include/chttpd.hrl").
 
