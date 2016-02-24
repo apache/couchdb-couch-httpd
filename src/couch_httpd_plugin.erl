b68bfd1ea8679d951a42efa10cf321f2a9b3d060
ILYA Khlopotov <iilyak@ca.ibm.com>
2016-02-24 07:07:05 -0800

    Rename chttpd_plugin into couch_httpd_plugin

--- src/couch_httpd_plugin.erl
+++ src/couch_httpd_plugin.erl
@@ -10,7 +10,7 @@
 % License for the specific language governing permissions and limitations under
 % the License.
 
--module(chttpd_plugin).
+-module(couch_httpd_plugin).
 
 -export([
     before_request/1,
